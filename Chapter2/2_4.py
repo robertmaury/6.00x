c143c7f77e12d97a2d96299aa98b97a944f6a260
Robert Maury <robert.maury@oracle.com>
2013-02-11 09:59:40 -0500

    Syncing changes from Pro

--- Chapter2/2_4.py
+++ Chapter2/2_4.py
@@ -1,1 +1,15 @@
-print 'testing. testing, 1...2....3....'
+i = 10
+odd = 0
+
+while (i > 0):
+    number = int(raw_input('Please enter integer: '))
+    if odd == 0 and number % 2 == 1:
+        greatest = number
+        odd = 1
+    elif odd == 1 and number > greatest and number % 2 == 1:
+        greatest = number
+    i = i - 1
+if odd == 0:
+    print 'No odd integers were entered'
+else:
+    print 'The largest odd integer entered was ' + str(greatest)
