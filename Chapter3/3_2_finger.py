46b71bc9f5bb8c5b3164606b116cc5071e494c27
robertmaury <robert.maury@oracle.com>
2013-02-12 21:08:13 -0500

    3_2 finger exercise completed

--- Chapter3/3_2_finger.py
+++ Chapter3/3_2_finger.py
@@ -1,13 +1,11 @@
 s = str(raw_input('Please enter a string of numbers, comma delimited: '))
 summation = 0
-for i in range(0, len(s) - 1):
-    number = s[i]
-    while True:
-        if s[i + 1] == ',':
-            i = i + 2
-            summation = summation + float(number)
-            break
-        else:
-            i = i + 1
-            number = number + s[i]
+number = ''
+for i in range(0, len(s)):
+    if s[i] == ',':
+        summation += float(number)
+        number = ''
+    else:
+        number = number + s[i]
+summation += float(number)
 print 'The sum of your values is ' + str(summation)
