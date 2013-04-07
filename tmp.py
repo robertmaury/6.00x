e264d6e00ae1ca3533d8913b74499610230ff704
robertmaury <robert.maury@oracle.com>
2013-04-07 14:09:19 -0400

    PS7

--- tmp.py
+++ tmp.py
@@ -1,16 +1,86 @@
+class RectangularRoom(object):
+    """
+    A RectangularRoom represents a rectangular region containing clean or dirty
+    tiles.
 
-import string
+    A room has a width and a height and contains (width * height) tiles. At any
+    particular time, each of these tiles is either clean or dirty.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes a rectangular room with the specified width and height.
 
-def storySplit(story):
-    tempStory = ''
-    punc = string.punctuation
-    for letter in story.lower():
-        if letter in punc:
-            tempStory += ' '
-        else:
-            tempStory += letter
-    print tempStory
-    return tempStory.split()
+        Initially, no tiles in the room have been cleaned.
 
-print storySplit("Hello. I love you. Won't you tell me your name")
+        width: an integer > 0
+        height: an integer > 0
+        """
+        self.width, self.height = width, height
+        self.canvas = {}
+        for i in range(width):
+            for j in range(height):
+                self.canvas[(i, j)] = False
+    
+    def cleanTileAtPosition(self, pos):
+        """
+        Mark the tile under the position POS as cleaned.
 
+        Assumes that POS represents a valid position inside this room.
+
+        pos: a Position
+        """
+        self.canvas[(int(pos[0]), int(pos[1]))] = True
+
+    def isTileCleaned(self, m, n):
+        """
+        Return True if the tile (m, n) has been cleaned.
+
+        Assumes that (m, n) represents a valid tile inside the room.
+
+        m: an integer
+        n: an integer
+        returns: True if (m, n) is cleaned, False otherwise
+        """
+        return self.canvas(m, n)
+    
+    def getNumTiles(self):
+        """
+        Return the total number of tiles in the room.
+
+        returns: an integer
+        """
+        return self.width * self.height
+
+    def getNumCleanedTiles(self):
+        """
+        Return the total number of clean tiles in the room.
+
+        returns: an integer
+        """
+        count = 0
+        for bool in self.canvas.values():
+            if bool == True:
+                count += 1
+        return count
+
+    def getRandomPosition(self):
+        """
+        Return a random position inside the room.
+
+        returns: a Position object.
+        """
+        x = random.random() * self.width
+        y = random.random() * self.height
+        return (x, y)
+
+    def isPositionInRoom(self, pos):
+        """
+        Return True if pos is inside the room.
+
+        pos: a Position object.
+        returns: True if pos is in the room, False otherwise.
+        """
+        return pos[0] <= self.width and pos[1] <= self.height
+
+room = RectangularRoom(5, 7)
+print room.getNumCleanedTiles()
